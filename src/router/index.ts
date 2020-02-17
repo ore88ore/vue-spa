4e32377af05dcc7d88f22ac72743f967c7911e32
Yusuke Sakai <sakai.yusuke@classmethod.jp>
2020-02-18 01:12:54 +0900

    feat: Vuetifyを追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Test from "@/views/Test.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -17,13 +18,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/test',
+    component: Test
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
---
+++ src/views/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Test Component</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "Test.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
